24e564f6a0317d0ca098e4464721b61fabb1c87c
Thenuka Ovin Weerasinghe <39676588+ov1n@users.noreply.github.com>
2023-08-20 03:04:23 +0530

    Added query for get_flights

--- lib/services/get_flights.dart
+++ lib/services/get_flights.dart
@@ -1,16 +1,26 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import '../../models/trip.dart';
+import '../../models/enums/craft_type.dart';
+import '../models/enums/cabin_type.dart';
 
 CollectionReference flights =
         FirebaseFirestore.instance.collection("FLIGHTS");
 
-Future<void> getFlights() {
-  // Call the user's CollectionReference to add a new user
-  return flights.get().then((event) {
-    for (var doc in event.docs) {
-      print("${doc.id} => ${doc.data()}");
-    }
-  })
-  .then((value) => print("Got flight"))
-          .catchError((error) => print("Failed to retrieve flight: $error"));
+Future<void> getFlights({String fromPort = "EARTH-007",String toPort = "MARS-001",
+  CraftType shuttleType = CraftType.Cruise, CabinType cabinType = CabinType.Economy,
+  int noOfPassengers = 1}) {
+
+    return flights
+      .where("from_port",isEqualTo:fromPort)
+      .where("to_port",isEqualTo:toPort)
+      .where("craft.craft_type",isEqualTo:shuttleType.toString().split('.').last)
+      .where("availabe_econ_seats",isGreaterThanOrEqualTo:noOfPassengers)
+        .get().then((event) {
+          for (var doc in event.docs) {
+            print("${doc.id} => ${doc.data()}");
+          }
+        })
+      .then((value) => print("Got flight"))
+        .catchError((error) => print("Failed to retrieve flight: $error"));
 }
--- lib/widgets/global/book_btn.dart
+++ lib/widgets/global/book_btn.dart
@@ -3,12 +3,17 @@ import 'package:astro_hub/screens/no_available_crafts.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
+import '../../services/get_flights.dart';
+
 Widget bookFlight(context) {
   return FloatingActionButton(
     backgroundColor: AppColors.primarySolid01,
     shape: const CircleBorder(),
     onPressed: (() {
       Navigator.push(context, MaterialPageRoute(builder: (context) {
+        // need to process context
+        Future flights = getFlights();
+        print(flights);
         return const NoAvailableCrafts();
       }));
     }),
