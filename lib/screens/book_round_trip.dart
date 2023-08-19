f2e07f2dff48df6089af29f7f708fbfc6f725dc9
Thenuka Ovin Weerasinghe <39676588+ov1n@users.noreply.github.com>
2023-08-20 01:33:49 +0530

    Fixed imagepaths

--- lib/screens/book_round_trip.dart
+++ lib/screens/book_round_trip.dart
@@ -2,6 +2,7 @@ import 'package:astro_hub/screens/home.dart';
 import 'package:astro_hub/widgets/global/common_app_bar.dart';
 import 'package:astro_hub/widgets/global/common_tabbar.dart';
 import 'package:flutter/material.dart';
+import '../constants/icon_paths.dart' as icons;
 
 class BookRoundTrip extends StatefulWidget {
   const BookRoundTrip({Key? key}) : super(key: key);
@@ -15,16 +16,18 @@ class BookRoundTripState extends State<BookRoundTrip> {
   Widget build(BuildContext context) {
 
     return const Scaffold(
-        appBar: CommonAppBar(title: 'Book A Trip', isBackButton: true),
-        body: CommonTabbar(
-            tab1: 'Round Trip',
-            tab2: 'One Way',
-            tab3: 'Multi Planet',
-            tab1Widget: Home(),
-            tab2Widget: Home(),
-            tab3Widget: Home(),
-            tab1img: 'assets/icons/round_trip.svg',
-            tab2img: 'assets/icons/one_way.svg',
-            tab3img: 'assets/icons/multi_planet.svg'));
+      appBar: CommonAppBar(title: 'Book A Trip', isBackButton: true),
+      body: CommonTabbar(
+          tab1: 'Round Trip',
+          tab2: 'One Way',
+          tab3: 'Multi Planet',
+          tab1Widget: Home(),
+          tab2Widget: Home(),
+          tab3Widget: Home(),
+          tab1img: icons.roundTrip,
+          tab2img: icons.oneWay,
+          tab3img: icons.multiPlanet
+      )
+    );
   }
 }
