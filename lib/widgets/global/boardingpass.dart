79280f6b077cd05a9c6d1fd04b1cfde106817790
Nilukshan Krishnaram <knilukshan@gmail.com>
2023-08-20 02:55:35 +0530

    Boarding Pass Modification (Imcomplete)

--- lib/widgets/global/boardingpass.dart
+++ lib/widgets/global/boardingpass.dart
@@ -5,16 +5,49 @@ import 'package:flutter_svg/svg.dart';
 Widget boardingpass(BuildContext context) {
   final height = MediaQuery.of(context).size.height;
   final width = MediaQuery.of(context).size.width;
-  return Container(
-    child: Container(
-      
-    ),
-    width: width*0.8,
-    decoration: const BoxDecoration(
-      image: DecorationImage(
-          image: AssetImage("assets/images/boardingpass_bg.png"),
-          fit: BoxFit.contain),
-    ),
+  return Column(
+    children: [
+      Image.asset(
+        'assets/images/boardingpass_bg.png', // Replace with your image path
+        fit: BoxFit.contain,
+        width: width*0.8,
+        // height: double.infinity,
+      ),
+      Container(
+        width: width*0.8,
+        color: Colors.black.withOpacity(0.5), // Semi-transparent overlay
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                'Ticket Number',
+                style: TextStyle(
+                  color: Color(0xFF565656),
+                  fontSize: 10,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+              // const SizedBox(height: 16.0),
+              const Text(
+                '1234567',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(height: 32.0),
+              Image.asset(
+                'assets/images/nasalogo.png', // Replace with your logo image path
+                width: 100.0,
+                height: 100.0,
+              ),
+            ],
+          ),
+        ),
+      ),
+    ],
   );
 }
-
